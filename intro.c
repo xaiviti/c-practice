f0f76f51a9452eb6cc0f0d25a4f187246ca0fc91
xaiviti <heidzie.siapco@gmail.com>
2022-01-12 23:31:29 +0800

    second commit

---
+++ intro.c
@@ -0,0 +1,23 @@
+/*
+C Programming is a compiled language
+    This means that all syntax has to be legal in order to create an executable file
+C is one of the oldest languages still used today
+    Inspired numerous other languages (C++, C#, Java, etc)
+C is not object oriented
+C is statistically typed (all variables are given a type at compile time)
+C is not forgiving - Simple mistakes are not told to you
+C is dangerous but helps you learn the details and behind the scenes
+C is valuable - Still in demand, foundational concepts help everyone
+C is hard - Highest learning curve second to assembly
+C is not TOO hard - Once you get the hang of it :)
+C is low level - Console apps and working with memory directly
+C is a very popular language for system programmers or low level programmers
+C has all the same programming structures used in other languages
+    -   Variables and data types
+    -   Operators
+    -   Control flow statements
+    -   Arrays and Strings
+    -   Functions
+    -   Structs
+    -   And some additional goodies: Pointers and Memory
+*/
