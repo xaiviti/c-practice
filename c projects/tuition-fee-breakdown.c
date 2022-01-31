cb99918777e88fb7fd8d41faa2930ceec959ea4c
xaiviti <heidzie.siapco@gmail.com>
2022-01-31 14:41:27 +0800

    basic-tuition-fee-calculator-commit2

--- c projects/tuition-fee-breakdown.c
+++ c projects/tuition-fee-breakdown.c
@@ -16,6 +16,7 @@ Total Tuition Fees and its Breakdown
 main(){
 	//declaration
 	double units, amountPerUnit, baseTuitionFee, modeOfPayment;
+	double tuitionFullPayment, tuitionInstallmentDP, tuitionInstallmentTerms;
 	
 	printf("Tuition Fee Calculator \n");
 	printf("-------------------------------- \n");
@@ -28,7 +29,27 @@ main(){
 	printf("Enter the amount per Unit: ");
 	scanf("%lf", &amountPerUnit);
 	baseTuitionFee = units * amountPerUnit;
+	double miscellaneous = 11589.70;
 	printf("Base Tuition is %lf", baseTuitionFee);
 	printf("Enter the mode of payment: ");
 	scanf("%f", &modeOfPayment);
+	switch(modeOfPayment){
+		case 1:
+			tuitionFullPayment = baseTuitionFee + miscellaneous;
+			printf("-------------------------------------------- \n");
+			printf("Total Tuition Fee: %lf\n", tuitionFullPayment);
+			printf("-------------------------------------------- \n");
+			printf("Breakdown: ");
+			printf("---------------------------- \n");
+			printf("Number of Units: &lf", units);
+			printf("Amount per Unit: %lf", amountPerUnit);
+			printf("Miscellaneous: %lf", amountPerUnit);
+			printf("---------------------------- \n");
+			break;
+		case 2:
+			break;
+		default:
+			break;
+		
+	}
 }
