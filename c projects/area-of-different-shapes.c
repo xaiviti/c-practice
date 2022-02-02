01d2bf9f41377053904c618a234be2fae9d8fd5a
xaiviti <heidzie.siapco@gmail.com>
2022-02-02 21:48:51 +0800

    area-of-different-shapes-commit3

--- c projects/area-of-different-shapes.c
+++ c projects/area-of-different-shapes.c
@@ -11,8 +11,8 @@
 
 main(){
 	//declaration
-	int choice;
-	double areaCircle, radius, diameter, measurementChoice;
+	int choice, measurementChoice;
+	double areaCircle, radius, diameter;
 	double areaSquare, side;
 	double areaRectangle, length, width;
 	double areaTriangle, side1, side2, side3, s;
@@ -29,10 +29,55 @@ main(){
 	//input 2 and process
 	switch(choice){
 		case 1:
+			printf("----------------------------------------\n");
+			printf("You have selected Circle\n");
+			printf("----------------------------------------\n");
+			printf("(1) Radius\n");
+			printf("(2) Diameter\n");
+			printf("Enter which measurement you have: \n");
+			scanf("%lf", &measurementChoice);
+			printf("----------------------------------------\n");
+			switch(measurementChoice){
+				case 1:
+					printf("Enter measurement of radius: \n");
+					scanf("%lf", &radius);
+					areaCircle = (3.1416)*(radius)*(radius);
+					printf("----------------------------------------\n");
+					printf("Area of Circle is  %lf\n", areaCircle);
+					break;
+				case 2:
+					printf("Enter measurement of diameter: \n");
+					scanf("%lf", &diameter);
+					areaCircle = ((3.1416)*(diameter)*(diameter))/4;
+					printf("----------------------------------------\n");
+					printf("Area of Circle is  %lf\n", areaCircle);
+					break;
+				default:
+					printf("Invalid Selection!");
+					break;
+			}
 			break;
 		case 2:
+			printf("----------------------------------------\n");
+			printf("You have selected Square\n");
+			printf("----------------------------------------\n");
+			printf("Enter measurement of side: \n");
+			scanf("%lf", &side);
+			areaSquare = side*side;
+			printf("----------------------------------------\n");
+			printf("Area of Square is  %lf\n", areaRectangle);
 			break;
 		case 3:
+			printf("----------------------------------------\n");
+			printf("You have selected Rectangle\n");
+			printf("----------------------------------------\n");
+			printf("Enter measurement of length: \n");
+			scanf("%lf", &length);
+			printf("Enter measurement of width: \n");
+			scanf("%lf", &width);
+			areaRectangle = length*width;
+			printf("----------------------------------------\n");
+			printf("Area of Rectangle is  %lf\n", areaRectangle);
 			break;
 		case 4:
 			printf("----------------------------------------\n");
