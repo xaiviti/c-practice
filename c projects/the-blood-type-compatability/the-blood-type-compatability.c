d2b73986d677564ab4f8d768c6efbe796f97fbff
xaiviti <heidzie.siapco@gmail.com>
2022-02-03 00:54:44 +0800

    the-blood-type-compatability

---
+++ c projects/the-blood-type-compatability/the-blood-type-compatability.c
@@ -0,0 +1,29 @@
+/*
+Blood Compatability
+*/
+
+main(){
+	//declaration
+	int choice, sign;
+	char bloodType;
+	
+	
+	//input
+	printf("BLOOD COMPATABILITY\n");
+	printf("------------------------------------\n");
+	printf("(1) Will Donate\n");
+	printf("(2) Will Receive\n");
+	printf("Select if you will donate or receive: ");
+	scanf("%d", &choice);
+	printf("Enter the Blood Type (capital letters): ");
+	scanf("%s", &bloodType);
+	printf("------------------------------------\n");
+	printf("(1) Positive\n");
+	printf("(2) Negative\n");
+	printf("------------------------------------\n");
+	printf("Enter the Blood Type: ");
+	scanf("%d", &sign);
+	
+	//process and output
+	
+}
