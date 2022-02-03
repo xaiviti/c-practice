2de5ca33a5eedd953e0743332c4af203ec288b19
xaiviti <heidzie.siapco@gmail.com>
2022-02-03 17:21:34 +0800

    the-dog-food-serving-recommendations

---
+++ c projects/the-dog-food-serving-recommendation/the-dog-food-serving-recommendation.c
@@ -0,0 +1,21 @@
+/*
+Dog Food Serving Recommendations
+*/
+
+main(){
+	//declaration
+	int dogSize;
+	double dogWeight;
+	
+	//input
+	printf("-------------DOG FOOD SERVING RECOMMENDATION-------------\n");
+	printf("(1) Toy\n");
+	printf("(2) Small\n");
+	printf("(3) Medium\n");
+	printf("(4) Large\n");
+	printf("Select your dog's size: ");
+	scanf("%d", &dogSize);
+	
+	//process and output
+	
+}
