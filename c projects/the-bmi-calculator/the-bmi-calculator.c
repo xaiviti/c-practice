c3934ddc71a8ee7e4c41aaad018d0e4eda427321
xaiviti <heidzie.siapco@gmail.com>
2022-02-03 16:14:36 +0800

    the-bmi-calculator-commit2

--- c projects/the-bmi-calculator/the-bmi-calculator.c
+++ c projects/the-bmi-calculator/the-bmi-calculator.c
@@ -17,20 +17,26 @@ main(){
 	
 	//process and output
 	printf("------------------------------------------------\n");
-	BMI = (weight*weight)/(height*height);
+	BMI = (weight)/(height*height);
 	printf("Your BMI is %lf.\n", &BMI);
 	if(BMI<18.5){
-		printf("You are Underweight.\n");
+		printf("You are Underweight. \n");
+		printf("Risk of developing health problems is increased. \n");
 	}else if(18.5<=BMI<=24.9){
 		printf("You are Normal Weight.\n");
+		printf("Risk of developing health problems is least. \n");
 	}else if(25.0<=BMI<=29.9){
 		printf("You are Overweight.\n");
+		printf("Risk of developing health problems is increased. \n");
 	}else if(30.0<=BMI<=34.9){
 		printf("You have Obesity class I.\n");
+		printf("Risk of developing health problems is high. \n");
 	}else if(35.0<=BMI<=39.9){
 		printf("You have Obesity class II.\n");
+		printf("Risk of developing health problems is very high. \n");
 	}else{
 		printf("You have Obesity class III.\n");
+		printf("Risk of developing health problems is extremely high. \n");
 	}
 	
 }
