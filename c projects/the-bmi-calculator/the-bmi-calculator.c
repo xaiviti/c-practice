338c4b63972e87dbd9d3c99db5b5516b3fdc39a1
xaiviti <heidzie.siapco@gmail.com>
2022-02-03 16:03:53 +0800

    the-bmi-calculator

---
+++ c projects/the-bmi-calculator/the-bmi-calculator.c
@@ -0,0 +1,36 @@
+/*
+BMI Calculator
+	- weight
+	- height
+*/
+
+main(){
+	//declaration
+	double BMI, weight, height;
+	
+	//input
+	printf("-----------------BMI CALCULATOR-----------------\n");
+	printf("Enter your weight(in kilograms): ");
+	scanf("%lf", &weight);
+	printf("Enter your height(in meters): ");
+	scanf("%lf", &height);
+	
+	//process and output
+	printf("------------------------------------------------\n");
+	BMI = (weight*weight)/(height*height);
+	printf("Your BMI is %lf.\n", &BMI);
+	if(BMI<18.5){
+		printf("You are Underweight.\n");
+	}else if(18.5<=BMI<=24.9){
+		printf("You are Normal Weight.\n");
+	}else if(25.0<=BMI<=29.9){
+		printf("You are Overweight.\n");
+	}else if(30.0<=BMI<=34.9){
+		printf("You have Obesity class I.\n");
+	}else if(35.0<=BMI<=39.9){
+		printf("You have Obesity class II.\n");
+	}else{
+		printf("You have Obesity class III.\n");
+	}
+	
+}
